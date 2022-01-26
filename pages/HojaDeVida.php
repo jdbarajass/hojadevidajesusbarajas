6dab283fcdd7c9f2fb706959f458f0f9b0a892fd
jdbarajass <jdbarajass@gmail.com>
2022-01-25 20:44:40 -0500

    Desplegar pagina web

---
+++ pages/HojaDeVida.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://kit.fontawesome.com/d4cd33a1a3.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../styles/styles.css" />
+    <link rel="stylesheet" href="../styles/stylesPersonajesBuenos.css" />
+    <link rel="stylesheet" href="../styles/stylesPersonajesMalos.css" />
+    <link rel="icon" href="../media/FotoBuena.ico" />
+    <script type="text/javascript" src="./script.js"></script>
+    <title>HojaDeVida</title>
+  </head>
+  <body>
+    <div class="columnas">
+        <div class="sidebar">
+        <div class="tituloNombreMio ">
+          José De Jesús <br />
+          Barajas Sotelo
+        </div>
+        <div class="bordeimagen">
+          <img
+            class="radioimagen"
+            src="../media/FotoBuena.png"
+            alt="Foto De Perfil"
+          />
+        </div>
+        <div class="Datos">
+          <div class="titulosDescriptivos">
+            <h2 class="movertitulo titulosTecnologia">Habilidades</h2>
+          </div>
+           <ul>
+            <li>
+              <h3>Dominio De Java Script, Java, Python CSS3 HTML5</h3>
+            </li>
+            <li>
+              <h3>Conocimiento De Las Bases De Datos</h3>
+            </li>
+            <li>
+              <h3>MongoDB</h3>
+            </li>
+            <li>
+              <h3>Capacidad De Liderazgo Y Trabajo En Equipo</h3>
+            </li>
+          </ul>           
+          <div class="titulosDescriptivos">
+            <h2 class="titulosTecnologia">Tecnologías</h2>
+          </div>
+          <div class="unioncolumnas">
+            <img
+              class="imagenReact"
+              src="../media/HTML5.png"
+              alt="Imagen React"
+            />
+            <img
+              class="imagenReact"
+              src="../media/node-js.png"
+              alt="Imagen React"
+            />
+            <img
+              class="imagenReact"
+              src="../media/CSS3.png"
+              alt="Imagen React"
+            />
+          </div>
+          <div class="separadodetextotecnologias">
+            <span>HTML5</span>
+            <span class="separadodetextotecnologias2">NodeJS</span>
+            <span class="separadodetextotecnologias2">CSS3</span>
+          </div>
+          <div class="unioncolumnas">
+            <img
+              class="imagenReact"
+              src="../media/TypeScript.png"
+              alt="Imagen React"
+            />
+            <img
+              class="imagenReact"
+              src="../media/mongodb.png"
+              alt="Imagen React"
+            />
+            <img
+              class="imagenReact"
+              src="../media/JavaScript.png"
+              alt="Imagen React"
+            />
+          </div>
+          <div class="separadodetextotecnologias">
+            <span>TypeScript</span>
+            <span class="separadodetextotecnologias">MongoDB</span>
+            <span class="separadodetextotecnologias">JavaScript</span>
+          </div>
+          <div >
+            <div>
+              <div class="titulodecontacto">
+
+                <h2 class="movertitulo titulosTecnologia">Contacto</h2>
+              </div>
+              <div iv>
+              <i class="fas fa-phone-alt telefonounion"><h3 class="telefonounion">Teléfono</h3></i>
+            </div>          
+            <h3 class="margin-left-telefono">(+57) 3144065520</h3>
+            <i class="far fa-envelope-open telefonounion">
+            <h3 class="telefonounion">jdbarajass@gmail.com</h3>
+            </i>
+            <div class="sepadodesitiosweb">
+            <h2>Sitios Web</h2>
+          </div>
+          <i class="fab fa-linkedin telefonounion sepadodesitiosweb">
+            <h3 class="telefonounion">
+              <a class="color-a-sitios-web" href="https://www.linkedin.com/in/jdbarajass/">LinkedIn</a></h3>
+          </i>
+          <i class="fab fa-github telefonounion sepadodesitiosweb">
+            <h3 class="telefonounion">
+              <a class="color-a-sitios-web" href="https://github.com/jdbarajass">GitHub</a>
+              </h3>
+          </i>
+          <div class="margin-top">
+            <i class="fas fa-map-marker-alt ubicacionunion">
+              <h3 class="ubicacionunion ubicacionunion2">
+                Colombia-Bogotá D.C
+              </h3>
+            </i>
+          </div>
+          </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div class="marginleftperfil">
+          <h1>Perfil</h1>
+        </div>
+        <h1 class="DescripcionCV justificartexto">
+          Ingeniero En Control Y Automatización egresado de la universidad distrital francisco José de caldas, de Bogotá, Colombia; Tengo conocimientos en electrónica, instrumentación, automatización y control; manejo lenguajes de programación como C++, Java, LabVIEW, Python, Matlab, R studio, JavaScript para desarrollo de páginas WEB con tecnologías como React, HTML5, NodeJS, MongoDB, CSS3 entre otros. Tambien tengo Conocimiento de PLC´S Siemens, actualmente haciendo proyectos de Domótica, cursos de programación, Diplomado en programación en la universidad de Antioquia y Beca por medio de BIT Bogotá Institute Of Technology, mantenimiento de cómputo, Soporte sistemas operativos y diferentes Windows, ofimática, conocimiento básico en Linux, diseño de páginas web, técnicas empresariales de digitación.
+
+        </h1>
+        <div class="titulosDescriptivos">
+          <h1 class="marginleft">Experiencia</h1>
+        </div>        
+         <div class="columnas">
+            <div class="fechaexperiencia">2018- <br> 2019</div>
+            <div class="DescripcionEmpresa">
+              <h2 class="quitarmargin">Programador De Robots De Sumo</h2>
+              <h3 class="colocarmarginbottom">Ja-Bots</h3>
+              <ul>
+                <li class="justificartexto">Programador enfocado a las competencias de Robots en la categoría de sumo. Se programan los diferentes Robots en el programa Arduino y con la tarjeta de desarrollo MiniBlack, para tener las diferentes tácticas en la competición y poder ganar al contrincante con las diferentes programaciones plasmadas como rutinas estratégicas. Se usa programación java orientada a objetos.</li>
+                  <li>Soldar los diferentes componentes electrónicos tarjetas controladoras y demás para su funcionamiento circuital.</li>
+                  <li>Ensamble de todas las piezas del robot para su correcto funcionamiento.</li>                  
+              </ul>            
+            </div>
+        </div>
+          <div class="columnas margin-top-a-titulos-educacion">
+            <div class="fechaexperiencia">2018- <br> 2019</div>
+            <div class="DescripcionEmpresa">
+              <h2 class="quitarmargin">Interventor</h2>
+              <h3 class="colocarmarginbottom">Area De Salud</h3>
+              <ul>
+                <li >Interventor en el área de salud para equipos de comunicación</li> 
+              <li class="justificartexto">Realizar visitas regularmente a los puestos de salud y verificar los avances de las diferentes sedes del proyecto, realizando informe para las mismas, verificar la entrega y la instalación, configuración y puesta en marcha para los equipos entregados durante el proyecto (Access Point, switch capa 2, servidores, firewall, nodo de hiperconvergencia, UPS, tablero red regulada y puntos de red), además de realizar el levantamiento de las actas para la entrega de los equipos.</li>
+              </ul>            
+            </div>
+        </div>
+         <div class="columnas margin-top-a-titulos-educacion">
+            <div class="fechaexperiencia">2018- <br> 2019</div>
+            <div class="DescripcionEmpresa">
+              <h2 class="quitarmargin">Programador y Diseñador 3D</h2>
+              <h3 class="colocarmarginbottom">Artmotics (Emprendimiento)</h3>
+              <ul>
+                <li>Programador de tarjetas NodeMCU, ESP8266, Módulo ESP-12E y sus diferentes gamas, todo enfocado hacia el sector domótico.</li> 
+              <li>Mantenimiento e instalación de cámaras de seguridad y cableado interno.</li>
+              <li>Diseño 3D en SolidWorks.(Manejo de maquina 3D)</li>
+              <li>Diseño De Circuitos Electrónicos En Eagle y Proteus.</li>
+              <li>Mantenimiento De Equipos De Computo.</li>
+              </ul>            
+            </div>
+        </div>
+          <div class="columnas margin-top-a-titulos-educacion">
+            <div class="fechaexperiencia">2018- <br> 2019</div>
+            <div class="DescripcionEmpresa">
+              <h2 class="quitarmargin">Ingeniero En Reparación Electrónica</h2>
+              <h3 class="colocarmarginbottom">Área De Telecomunicaciones</h3>
+              <ul>
+                <li class="justificartexto">Realizar diagnóstico y de ser posible la reparación de los equipos (módem y decodificadores de video). en el área de Re-manufacturación para las principales empresas que prestan el servicio de televisión y telefonía hogar (Claro, Une, Tigo, ETB) .</li>
+              </ul>            
+            </div>
+        </div>
+          <div class="columnas margin-top-a-titulos-educacion">
+            <div class="fechaexperiencia">2018- <br> 2019</div>
+            <div class="DescripcionEmpresa">
+              <h2 class="quitarmargin">Universidad Distrital Francisco José De Caldas</h2>
+              <h3 class="colocarmarginbottom">Monitor De Laboratorio en la sala de Ciencias Básicas Y Software</h3>
+              <ul>
+                <li>Asistencia a los diferentes docentes sobre informática.</li>
+                <li>Manejo de Matlab y capacitación de la misma a los estudiantes.</li>
+                <li>Manejo de tablero electrónico para disposición de los docentes.</li>
+                <li>Manejo de equipo multimedia como lo es video Beam, audio ETC.</li>
+              </ul>            
+            </div>
+        </div>          
+        <div class="titulosDescriptivos">
+          <h1 class="marginleft">Educación</h1>
+          </div>
+          <div class="columnas">
+            <div class="fechaexperiencia">2021- <br> 2022</div>
+            <div class="DescripcionEmpresa">
+              <h1 class="quitarmargin">Universidad Distrital Francisco José De Caldas</h1>
+              <h3 class="colocarmarginbottom">Ingenieria En Control y Autorización</h3>
+              <ul>
+                <li class="justificartexto">Ingeniero En Control Y Automatización con conocimientos en electrónica, instrumentación, automatización y control; manejo 
+              lenguajes de programación como C++, Java, LabVIEW, Python, Matlab, R studio, JavaScript para desarrollo de páginas WEB con tecnologías como React, HTML5, Nodejs, CSS3 entre otros. Conocimiento de PLC´S Siemens, actualmente haciendo proyectos de Domótica, cursos de programación, Diplomado en programación en la universidad de Antioquia y Beca por medio de BIT Bogotá Institute Of Technology, mantenimiento de cómputo, Soporte sistemas operativos y diferentes Windows, ofimática, conocimiento
+              básico en Linux, diseño de páginas web, técnicas empresariales de digitación.</li>
+              </ul>            
+            </div>
+        </div>
+        <div class="columnas margin-top-a-titulos-educacion">
+            <div class="fechaexperiencia">2021- <br> 2021</div>
+            <div class="DescripcionEmpresa">
+              <h1 class="quitarmargin">Diplomado en habilidades en programación con énfasis en aplicaciones Web.</h1>
+              <h3 class="colocarmarginbottom">Certificado Realidado En La Universidad De Antioquia (+800 Horas)</h3>
+              <ul>
+                <li> Lenguajes (JavaScript, TypeScript, Java, Python), Bases De Datos NoSQL (MongoDB) No Relacional, Integración Backend & Frontend (Stack MEAN / MERN), Contenedores Para Apps (Docker), Integración A GraphQL Con NodeJS, Despliegue en Entornos Cloud (PassS), Gestión De La Configuración Del Software (Git / GitHub / SourceTree) entre otros.</li> 
+              </ul>            
+            </div>
+        </div>
+        <div class="columnas margin-top-a-titulos-educacion">
+            <div class="fechaexperiencia">2021- <br> 2022</div>
+            <div class="DescripcionEmpresa">
+              <h1 class="quitarmargin">BIT (Bogotá Institute Of Technology)</h1>
+              <h3 class="colocarmarginbottom">BootCamp Desarrollo Web Full Stack</h3>
+              <ul>
+                <li>
+                Desarrollo FronEnd
+              </li>
+              <li>
+                Desarrollo BackEnd
+              </li>
+              <li>Frameworks DataBase</li>
+              <li>Desarrollo De Aplicaciones Web Mean Stack</li>
+              </ul>            
+            </div>
+        </div>
+        <div>
+
+        </div>
+        <div class="margindelpiedepagina">
+          <div class="columnas margin-top-a-titulos-educacion">
+              <div class="fechaexperiencia">2012- <br> 2013</div>
+              <div class="DescripcionEmpresa">
+                <h1 class="quitarmargin">Academia De Sistemas Nuevo Milenio.</h1>
+                <h3 class="colocarmarginbottom">Técnico en sistemas. (Técnico profesional en mantenimiento de computadores, técnicas empresariales de digitación)</h3>
+                <ul>
+                   <li>
+                  Mantenimiento De Equipos De Cómputo (Software y Hardware)
+                </li>
+                </ul>            
+              </div>
+          </div>     
+        </div>
+        <div class="titulosDescriptivos">
+          <h1 class="marginleft">Información Adicional</h1> 
+          </div>
+          <ul>
+            <li>
+              Permiso para conducir B1 y moto propia
+            </li>
+            <li>
+              Disponibilidad inmediata
+            </li>
+          </ul>
+  </body>
+</html>
